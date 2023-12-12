cdd7757f8ea0ae7a185989d71f07bef7a45f4f45
enginseven <32772405+enginseven@users.noreply.github.com>
2023-12-13 02:00:13 +0300

    Update db.py

--- app/db.py
+++ app/db.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from .models import Base, HastaMetin, HastaSorular#, HastaGozlem
 
-DATABASE_URL = "postgresql:5432//localhost:J1WDQBgrkC89NUC80FxiMX6fxsnTJ9Zn@dpg-clse4m63d79c739gn2pg-a:5432/postgres_db_jlut"
+DATABASE_URL = "postgres://localhost:J1WDQBgrkC89NUC80FxiMX6fxsnTJ9Zn@dpg-clse4m63d79c739gn2pg-a/postgres_db_jlut"
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
